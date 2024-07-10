c3bba0e9cf96a49e3c7703b47e455b5ad7091aec
qihang791 <qihang791@163.com>
2024-07-11 01:30:24 +0800

    营业额统计代码开发

---
+++ sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
@@ -0,0 +1,28 @@
+package com.sky.service.impl;
+
+import com.sky.mapper.OrderMapper;
+import com.sky.service.ReportService;
+import com.sky.vo.TurnoverReportVO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+
+@Service
+@Slf4j
+public class ReportServiceImpl implements ReportService {
+
+    @Autowired
+    private OrderMapper orderMapper;
+    /**
+     * 统计指定时间区间内的营业额数据
+     * @param begin
+     * @param end
+     * @return
+     */
+    public TurnoverReportVO getTurnoverStatistics(LocalDate begin, LocalDate end) {
+        return null;
+    }
+
+}
